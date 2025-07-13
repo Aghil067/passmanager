0aba4893157b34afd2ae540c5ab473904bdb3133
R <aghilesh26@gmail.com>
2025-07-13 11:53:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
-import Manager from './components/manager'
+import Manager from './components/Manager'
 import Footer from './components/Footer'
 
 function App() {
